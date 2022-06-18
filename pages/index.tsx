7de3085c5b78ec26d5f5ebc348cfa9ace74a359d
dev-frog <kali.nisga@gmail.com>
2022-06-19 02:13:33 +0600

    Update Readme File

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,7 @@ export async function getStaticProps() {
 
 
 const Home = ({images} : {images: Image[]}) => {
+  console.log({images});
   return (
     <div className='max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
       <div className='grid gird-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
