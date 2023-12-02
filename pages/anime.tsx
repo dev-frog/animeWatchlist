78adb27ef5976bf31a5754bf8374192026a1647a
dev-frog <kali.nisga@gmail.com>
2023-12-03 02:00:32 +0600

    Fix formatting in anime.tsx

--- pages/anime.tsx
+++ pages/anime.tsx
@@ -28,10 +28,10 @@ const anime = () => {
 
   return (
     <>
-      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <div className="max-w-7xl    mx-auto py-6 sm:px-6 lg:px-8">
         <div className="bg-white">
           <div className="mx-auto max-w-2xl py-5 px-4  sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8">
+            <div className="mt-6 grid grid-cols-1 gap-y-10   gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-8">
               {anime.map((product, index) => (
                 <div key={index} className="group relative">
                   <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
